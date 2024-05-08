959f092dc672ef5a20aa1bf39dc1275a65356e37
Noura Zaruali <noura.zaruali@gmail.com>
2024-05-08 13:40:09 +0200

    Code final TT

---
+++ AMD_Flash.X/build/default/debug/_ext/1889744778/Flash.o.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/1889744778/Flash.o: \
+ ../../Downloads/Sacha/Flash.c ../../Downloads/Sacha/Flash.h
+
+../../Downloads/Sacha/Flash.h:
---
+++ AMD_Flash.X/build/default/production/_ext/1889744778/Flash.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1889744778/Flash.o: \
+ ../../Downloads/Sacha/Flash.c ../../Downloads/Sacha/Flash.h
+
+../../Downloads/Sacha/Flash.h:
